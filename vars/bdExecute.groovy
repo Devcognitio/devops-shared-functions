7778ac36f42fbbb038958c82f3ade3bb2f316753
Jennifer Perez Bedoya <jenniferperezb@gmail.com>
2018-12-17 16:38:12 -0500

    Se prueba ejecucion de script de BD desde un directorio

--- vars/bdExecute.groovy
+++ vars/bdExecute.groovy
@@ -1,9 +1,14 @@
+import groovy.io.FileType
+
 def call(script) {
     node {
         withCredentials([usernamePassword(credentialsId: 'SQL_SERVER_CREDENTIALS',
                                           usernameVariable: 'USERNAME',
                                           passwordVariable: 'PASSWORD')]) {
-            sh "cat ${script} | sqlcmd -s localhost -u $USERNAME -p $PASSWORD -o 1433"
+            def dir = new File("/Users/jenniferperezbedoya/Documents/proyectos/ICProjects/jenkins/ServicioBase/bd")
+            dir.eachFileRecurse (FileType.FILES) { file ->
+                sh "cat ${file} | sqlcmd -s localhost -u $USERNAME -p $PASSWORD -o 1433"
+            }
         }
     }
 }
