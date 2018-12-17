ccb3afe9e85a0e1d44d308bf4680130db4d2819a
Jennifer Perez Bedoya <jenniferperezb@gmail.com>
2018-12-17 18:03:15 -0500

    Se prueba ejecucion de script de BD desde un directorio

--- vars/bdExecute.groovy
+++ vars/bdExecute.groovy
@@ -5,6 +5,7 @@ def call(directoryPath) {
         withCredentials([usernamePassword(credentialsId: 'SQL_SERVER_CREDENTIALS',
                                           usernameVariable: 'USERNAME',
                                           passwordVariable: 'PASSWORD')]) {
+            sh "pwd"
             def dir = new File(directoryPath)
             dir.eachFileRecurse (FileType.FILES) { file ->
                 sh "cat ${file} | sqlcmd -s localhost -u $USERNAME -p $PASSWORD"
