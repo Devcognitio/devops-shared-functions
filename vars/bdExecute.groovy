92297ac18637862dbeeb32f037572f816df010f8
Jennifer Perez Bedoya <jenniferperezb@gmail.com>
2018-12-17 13:25:02 -0500

    Se agregan credenciales obtenidas desde jenkins

--- vars/bdExecute.groovy
+++ vars/bdExecute.groovy
@@ -1,6 +1,6 @@
 def call(script) {
     node {
-        withCredentials([usernamePassword(credentialsId: 'bd-credenciales-desa',
+        withCredentials([usernamePassword(credentialsId: 'SQL_SERVER_CREDENTIALS',
                                           usernameVariable: 'USERNAME',
                                           passwordVariable: 'PASSWORD')]) {
             sh "cat ${script} | sqlcmd -s localhost -u $USERNAME -p $PASSWORD -o 1433"
