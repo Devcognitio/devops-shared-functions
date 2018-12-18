f12870e60dcb32cfce050054675f4ec4e3db4393
Jennifer Perez Bedoya <jenniferperezb@gmail.com>
2018-12-18 17:06:29 -0500

    manejando excepciones

--- vars/bdExecute.groovy
+++ vars/bdExecute.groovy
@@ -1,9 +1,5 @@
 import groovy.io.FileType
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import com.pe.suraam.functions.BDConfigReader
-
-def Logger logger = LoggerFactory.getLogger("devops-shared-functions-log")
 
 def call(scriptsPath, configFilePath) {
     withCredentials([usernamePassword(credentialsId: 'SQL_SERVER_CREDENTIALS',
@@ -13,14 +9,14 @@ def call(scriptsPath, configFilePath) {
         try {
             def dir = new File("${workspacePath}" + "${scriptsPath}")
             def config = BDConfigReader.readConfigFile("${workspacePath}" + "${configFilePath}")
-            echo("CONFIG FILE -> SKIP BD SCRIPTS EXECUTION: ${config.skipExecution}")
-            if(!config.skipExecution) {
-                dir.eachFileRecurse (FileType.FILES) { file ->
-                    sh "cat ${file} | sqlcmd -s localhost -u $USERNAME -p $PASSWORD"
-                }
-            }
         } catch(FileNotFoundException exc) {
-            logger.error("BD Scripts path, configuration file path or both are incorrect, please verify that exists and are not null")
+            throw new FileNotFoundException("BD Scripts path, configuration file path or both are incorrect, please verify that exists and are not null", exc.getMessage())
+        }
+        echo("CONFIG FILE -> SKIP BD SCRIPTS EXECUTION: ${config.skipExecution}")
+        if(!config.skipExecution) {
+            dir.eachFileRecurse (FileType.FILES) { file ->
+                sh "cat ${file} | sqlcmd -s localhost -u $USERNAME -p $PASSWORD"
+            }
         }
     }
 }
