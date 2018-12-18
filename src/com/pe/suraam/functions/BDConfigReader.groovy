c31c962b9f0c70d96d65ee91e4d0981160b807fd
Jennifer Perez Bedoya <jenniferperezb@gmail.com>
2018-12-18 15:05:09 -0500

    refactor sobre funcion que lee json

--- src/com/pe/suraam/functions/BDConfigReader.groovy
+++ src/com/pe/suraam/functions/BDConfigReader.groovy
@@ -9,7 +9,7 @@ class BDConfigReader {
         def inputFile = new File("${jsonPath}")
         //script.echo("Desde BDConfigReader")
         //script.sh "pwd"
-        echo "${inputFile}"
+        print "${inputFile}"
        // return new JsonSlurper().parseText(inputFile.text) as ConfigFile
         //return new JsonSlurper().parseText(inputFile.text)
     }
