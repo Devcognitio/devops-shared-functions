653a9c487c7a8c9dbfc4ceba023c4ba1e3c0a700
Jennifer Perez Bedoya <jenniferperezb@gmail.com>
2018-12-17 19:12:59 -0500

    Se prueba ejecucion de script de BD desde un directorio

--- vars/bdExecute.groovy
+++ vars/bdExecute.groovy
@@ -7,7 +7,7 @@ def call(directoryPath) {
             sh "pwd"
             sh "ls"
             def workspacePath = sh 'pwd'
-            def dir = new File("bd/scripts")
+            def dir = new File("/Users/jenniferperezbedoya/.jenkins/workspace/vicioBase_jennifers-feature-ZEELFDI7BN7A3AAPVNHISVJVERJ7OLHTGQS4JZX23YBYW5EEWYPA/bd/scripts")
             dir.eachFileRecurse (FileType.FILES) { file ->
                 sh "cat ${file} | sqlcmd -s localhost -u $USERNAME -p $PASSWORD"
             }
